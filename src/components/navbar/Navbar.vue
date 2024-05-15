8b3a4c05aea526c1f12503fdd94a8ffb3753d5c7
thivco <thibault.vivier@epitech.eu>
2024-05-15 18:13:10 +0200

    Adding dynamic routing

---
+++ src/components/navbar/Navbar.vue
@@ -0,0 +1,13 @@
+<script>
+let items = ["Home", "Gallery", "Gigs","About"]
+</script>
+
+<template>
+    <ul>
+        <li v-for="item in items">
+            {{ item }}
+        </li>
+    </ul>
+</template>
+
+<style></style>
--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,7 +1,18 @@
-<script>
+<script setup>
+import { ref } from 'vue'
+import Navbar from "../navbar/Navbar.vue"
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
 </script>
 
 <template>
+    <Navbar/>
+<div>
+    <h1>{{ msg }}</h1>
+</div>
 </template>
 
 <style>
