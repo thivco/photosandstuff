dc2198a1d0bbbded1373066075a56559726e3d15
thivco <thibault.vivier@epitech.eu>
2024-05-16 23:44:14 +0200

    Re-added the routes as vue data

--- src/components/navbar/Navbar.vue
+++ src/components/navbar/Navbar.vue
@@ -1,10 +1,27 @@
 <script>
-let items = ["Home", "Gallery", "Gigs","About"]
+
+import Home from "../pages/Home.vue"
+import Gigs from "../pages/Gigs.vue"
+import Contact from "../pages/Contact.vue"
+import Gallery from "../pages/Gallery.vue"
+
+let routes = {"/home": Home,
+"/gigs": Gigs,
+"/contact": Contact,
+"/gallery": Gallery,
+}
+
+export default {
+    data() {
+    routes
+  },
+}
+
 </script>
 
 <template>
     <ul>
-        <li v-for="item in items">
+        <li v-for="item in routes">
             {{ item }}
         </li>
     </ul>
